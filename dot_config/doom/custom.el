33ba658119ad22f571e299b790bff0e79422d8df
Daniel Xu <danielxu0307@proton.me>
2024-10-19 16:24:58 -0700

    remove thefuck

--- dot_config/doom/custom.el
+++ dot_config/doom/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/notes/org/s2024/refile.org" "/var/home/d/notes/org/s2024/school.org")))
+   '("~/notes/org/s2024/school.org" "/Users/d/notes/org/s2024/refile.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
