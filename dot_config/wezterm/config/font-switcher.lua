286a54376fcfa334696e59bf92b4623f9984dfb6
Daniel Xu <danielxu0307@proton.me>
2024-11-22 18:31:39 -0800

    wezterm

---
+++ dot_config/wezterm/config/font-switcher.lua
@@ -0,0 +1,99 @@
+local wezterm = require("wezterm")
+local io = require("io")
+
+---@class FontTable
+---@field family string
+---@field weight? string
+---@field harfbuzz_features? string[]
+
+---@alias Font FontTable | string
+
+---@class FontOption
+---@field font Font
+---@field size? number
+---@field command_palette_font Font
+
+local M = {}
+
+---@class Defaults
+---@field size? number
+---@field command_palette_font? Font
+
+---@type Defaults
+local defaults = {
+  size = 13,
+  command_palette_font = "IBM Plex Sans",
+}
+
+---@param config table<string, any>
+---@param option FontOption
+---@return table<string, any>
+local function apply_font_option(config, option)
+  config.font = wezterm.font(option.font)
+  config.font_size = option.size or defaults.size
+  config.command_palette_font_size = (option.size or defaults.size) * 1.1
+
+  local window_frame = config.window_frame or {}
+  window_frame.font =
+      wezterm.font(option.command_palette_font or defaults.command_palette_font)
+  config.window_frame = window_frame
+
+  return config
+end
+
+---@param config any
+---@param options {fonts: table<string, FontOption>}
+M.apply = function(config, options)
+  wezterm.GLOBAL.fonts = options.fonts
+
+  local last_font_fp = wezterm.home_dir .. "/.local/share/wezterm/font.json"
+  local last_font_fh = io.open(last_font_fp, "r")
+
+  if last_font_fh ~= nil then
+    local last_font = wezterm.json_parse(last_font_fh:read())
+    if last_font ~= nil then
+      apply_font_option(config, last_font)
+    end
+  end
+end
+
+local function selector_callback(window, _, font_id, _)
+  ---@type FontOption
+  local new_font = wezterm.GLOBAL.fonts[tonumber(font_id)]
+
+  local overrides =
+      apply_font_option(window:get_config_overrides() or {}, new_font)
+  window:set_config_overrides(overrides)
+
+  local fh =
+      io.open(wezterm.home_dir .. "/.local/share/wezterm/font.json", "w+")
+
+  if fh ~= nil then
+    fh:write(wezterm.json_encode(new_font)):close()
+  end
+end
+
+wezterm.on("switch-font", function(window, pane)
+  local font_choices = {}
+  ---@param id string
+  ---@param value FontOption
+  for id, value in ipairs(wezterm.GLOBAL.fonts) do
+    local id_string = tostring(id)
+    if type(value.font) == "userdata" then
+      table.insert(font_choices, { label = value.font.family, id = id_string })
+    else
+      table.insert(font_choices, { label = value.font, id = id_string })
+    end
+  end
+
+  window:perform_action(
+    wezterm.action.InputSelector({
+      action = wezterm.action_callback(selector_callback),
+      title = "Switch Font",
+      choices = font_choices,
+    }),
+    pane
+  )
+end)
+
+return M
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,18 +1,43 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
+local c = wezterm.config_builder()
+local utils = require("config.utils")
 
-config.color_scheme = "Catppuccin Mocha"
-config.window_decorations = "TITLE | RESIZE"
-config.enable_wayland = false
+require("config.font-switcher").apply(c, {
+  fonts = {
+    { font = "BlexMono Nerd Font" },
+    { font = "CaskaydiaMonoNF Nerd Font" },
+    { font = "IntoneMono Nerd Font" },
+    { font = "JetBrainsMono Nerd Font" },
+    { font = "MonaspiceAr Nerd Font" },
+    { font = "MonaspiceKr Nerd Font" },
+    { font = "MonaspiceNe Nerd Font" },
+    { font = "MonaspiceRn Nerd Font" },
+    { font = "MonaspiceXe Nerd Font" },
+  },
+})
+require("config.keys").apply(c)
+require("config.zen-mode")
 
-config.font = wezterm.font("Cascadia Code")
+c.front_end = "WebGpu"
+c.default_prog = { "/usr/bin/zsh", "-l" }
 
-config.font_size = 12
-config.default_prog = { "/usr/bin/zsh" }
-config.front_end = "WebGpu"
+if utils.is_darwin() then
+  c.macos_window_background_blur = 20
+  c.window_background_opacity = 0.95
+  c.window_decorations = "RESIZE|INTEGRATED_BUTTONS"
+  c.window_padding = { left = 0, right = 0, top = 50, bottom = 0 }
+else
+  c.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+  c.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+end
 
-config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = true
-config.hide_tab_bar_if_only_one_tab = true
+c.adjust_window_size_when_changing_font_size = false
+c.font_size = 14.0
+c.audible_bell = "Disabled"
+c.default_cursor_style = "BlinkingBar"
+c.inactive_pane_hsb = { brightness = 0.90 }
+c.color_scheme = "Catppuccin Mocha"
 
-return config
+require("bar.plugin").apply_to_config(c)
+
+return c
