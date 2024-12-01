0bb3bc3bab1d065f24cf58ad770dfba11c01f80d
Daniel Xu <danielxu0307@proton.me>
2024-11-30 22:12:59 -0800

    add bar padding

--- dot_config/nvim/lua/custom/plugins/colorscheme.lua
+++ dot_config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
-  'ellisonleao/gruvbox.nvim',
+  'shaunsingh/nord.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    vim.cmd.colorscheme 'gruvbox'
+    vim.cmd.colorscheme 'nord'
   end,
 }
