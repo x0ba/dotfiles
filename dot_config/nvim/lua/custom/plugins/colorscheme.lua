5692d90deb4b46c59a392e386d921f46c74eefc6
Daniel Xu <danielxu0307@proton.me>
2024-12-02 17:16:31 -0800

    catppuccin

--- dot_config/nvim/lua/custom/plugins/colorscheme.lua
+++ dot_config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,7 +1,8 @@
 return {
-  'ellisonleao/gruvbox.nvim',
+  'catppuccin/nvim',
+  name = 'catppuccin',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    vim.cmd.colorscheme 'gruvbox'
+    vim.cmd.colorscheme 'catppuccin'
   end,
 }
