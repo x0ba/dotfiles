5789f01938bf9de6856923a50a085ee99d621303
Daniel Xu <danielxu0307@proton.me>
2024-12-06 21:48:04 -0800

    add snacks.nvim

---
+++ home/dot_config/nvim/lua/custom/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    win = { enabled = true },
+    dashboard = { enabled = true },
+    words = { enabled = true },
+    scratch = { enabled = true },
+  },
+}
