44bf7d009c1dfe664c6454288d237166d3f27e4f
Daniel Xu <danielxu0307@proton.me>
2024-12-20 18:28:03 -0800

    use catppuccin mocha

--- home/dot_config/nvim/lua/daniel/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/daniel/plugins/colorscheme.lua
@@ -3,6 +3,6 @@ return {
   name = 'catppuccin',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    vim.cmd.colorscheme 'catppuccin-macchiato'
+    vim.cmd.colorscheme 'catppuccin'
   end,
 }
