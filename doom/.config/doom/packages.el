e197bc5c588b2985eb74b0a4fa644ee29b0fa4e6
Daniel Xu <danielxu0307@proton.me>
2025-04-03 22:31:34 -0700

    gruvbox

--- doom/.config/doom/packages.el
+++ doom/.config/doom/packages.el
@@ -47,5 +47,3 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
-
-(package! catppuccin-theme)
--- nvim/.config/nvim/lua/daniel/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/daniel/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    vim.cmd.colorscheme("catppuccin")
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  opts = {},
+  config = function()
+    vim.cmd([[colorscheme gruvbox]])
   end,
 }
