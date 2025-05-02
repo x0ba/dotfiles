374d105efd23111070a78f3864c46fd560900212
Daniel Xu <danielxu0307@proton.me>
2025-05-01 18:04:06 -0700

    kanagawa

--- nvim/.config/nvim/lua/daniel/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/daniel/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "rebelot/kanagawa.nvim",
   priority = 1000,
   opts = {},
   config = function()
-    vim.cmd([[colorscheme catppuccin]])
+    vim.cmd("colorscheme kanagawa")
   end,
 }
--- zsh/.config/zsh/.zshenv
+++ zsh/.config/zsh/.zshenv
@@ -4,4 +4,3 @@ export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CACHE_HOME="$HOME/.cache"
 export EDITOR='nvim'
 export MANPAGER='nvim +Man!'
-. "$HOME/.cargo/env"
